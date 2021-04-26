67bb171e968bf15596bedd967fbc5cd866e49f29
asahi <asap1017@docomo.ne.jp>
2021-04-26 10:45:09 +0900

    routes、validatesの定義 #13

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,3 +1,8 @@
 class Community < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { minimum: 2 }
+
+  validates :description, presence: true
+
   belongs_to :user
 end
